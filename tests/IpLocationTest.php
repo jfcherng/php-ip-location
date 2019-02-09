632617abf645f4e42ff6f8c98613316fb207a468
Jack Cherng <jfcherng@gmail.com>
2019-02-09 16:31:16 +0800

    nits
    Signed-off-by: Jack Cherng <jfcherng@gmail.com>

--- tests/IpLocationTest.php
+++ tests/IpLocationTest.php
@@ -17,13 +17,9 @@ class IpLocationTest extends TestCase
      */
     public static function setupBeforeClass(): void
     {
-        // 如果不想要使用內建的 IP 資料庫，請進行以下設定
         IpLocation::setup([
-            // ipip 資料庫的路徑
             'ipipDb' => __DIR__ . '/../src/db/ipipfree.ipdb',
-            // cz88 資料庫的路徑
             'cz88Db' => __DIR__ . '/../src/db/qqwry.dat',
-            // cz88 資料庫是否為 UTF-8 編碼
             'cz88DbIsUtf8' => false,
         ]);
     }
